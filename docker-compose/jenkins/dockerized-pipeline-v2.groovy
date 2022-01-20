2d739145fa2394246d27a8349740603e8a018012
MordechaiTamam <modi.tamam@gmail.com>
2022-01-20 20:38:11 +0200

    Added docker build stage

--- docker-compose/jenkins/dockerized-pipeline-v2.groovy
+++ docker-compose/jenkins/dockerized-pipeline-v2.groovy
@@ -2,6 +2,11 @@ pipeline {
     agent { label 'agent_1' }
 
     stages {
+         stage('Docker build'){
+            steps{
+                sh 'docker build -t hello-flask-160122 /external/<PATH-TO_YOUR_FLASK_DIR>'
+            }
+        }
         stage('API call') {
             agent {
                 docker {
